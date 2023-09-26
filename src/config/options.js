34c1c286b1d9bd85fb8024e8158be94e59ea8283
fl1jason <jason@fl1digital.com>
2023-09-26 23:19:31 +0100

    Delete src/config/options.js
    deleted file

--- src/config/options.js
+++
@@ -1,9 +0,0 @@
-const CHATGPT_PROMPT_TEXT = `Parse the following text and return a vCard: `;
-const CHATGPT_API_KEY = "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";
-const CHATGPT_MAX_TOKENS = 50;
-
-export default {
-  CHATGPT_PROMPT_TEXT,
-  CHATGPT_API_KEY,
-  CHATGPT_MAX_TOKENS,
-};
